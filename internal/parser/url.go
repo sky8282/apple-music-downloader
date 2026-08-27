933be25b4e93253d0c96b804ea1ce7c9824473df
sky828 <667515@qq.com>
2026-08-27 21:46:41 +0800

    Update url.go

--- internal/parser/url.go
+++ internal/parser/url.go
@@ -64,3 +64,14 @@ func CheckUrlArtist(url string) (string, string) {
 	}
 }
 
+// CheckUrlStation validates and extracts info from a Station URL
+func CheckUrlStation(url string) (string, string) {
+	pat := regexp.MustCompile(`^(?:https:\/\/(?:beta\.music|music)\.apple\.com\/(\w{2})(?:\/station|\/station\/.+))\/(?:id)?(ra\.[\w-]+)(?:$|\?)`)
+	matches := pat.FindAllStringSubmatch(url, -1)
+
+	if matches == nil {
+		return "", ""
+	} else {
+		return matches[0][1], matches[0][2]
+	}
+}
